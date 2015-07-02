720e77ee406a27f599d0860d94a322ea722ac76a
Nam Nguyen <namnguyen@R68Freds-Mac-mini.local>
2015-07-02 16:47:28 -0700

    made minor style changes. added contact info to contact section. added new project descriptions in the portfolio section

--- js/custom.js
+++ js/custom.js
@@ -7,12 +7,3 @@ $(document).scroll(function(){
     $('.top-bar').removeClass('scrolled');
   }
 });
-
-$("#map-info-toggle").hover(
-  function () {
-    $("#map-marker-info-box").animate({'opacity': '1'}, 400);
-  }, 
-  function () {
-    $("#map-marker-info-box").animate({'opacity': '0'}, 400);
-  }
-);
